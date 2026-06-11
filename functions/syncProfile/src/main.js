5cdb3b012d8fc3c774cb0fce54ad6a8baeb6a309
WinterSunset95 <wintersunset95@gmail.com>
2026-06-11 17:12:07 +0530

    feat: added more event handlers to the syncprofile function

--- functions/syncProfile/src/main.js
+++ functions/syncProfile/src/main.js
@@ -1,15 +1,13 @@
-import { Client, Users, Databases } from 'node-appwrite';
+import { Client, Users, Databases, TablesDB } from 'node-appwrite';
 
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
   const eventHeader = req.headers['x-appwrite-event'] ?? req.headers['x-test-event'] ?? '';
 
-  console.log("Event: ", eventHeader)
-
-  if (!eventHeader.includes('users.') || !eventHeader.includes('.create')) {
-    error(`Critical failure: Not a user creation event!`)
+  if (!eventHeader.includes('users.')) {
+    error(`Critical failure: Not a user event!`)
     return res.json({
-      error: "Not a user creation event!",
+      error: "Not a user event!",
       event: eventHeader
     })
   }
@@ -22,29 +20,47 @@ export default async ({ req, res, log, error }) => {
     return res.json({ success: false }, 400)
   }
 
-  log(`Intercepted new user signup: ${userId}. Syncing to database`)
+  log(`Intercepted new user event: ${userId}. Syncing to database`)
 
   const client = new Client()
     .setEndpoint('https://appwrite.wintersunset95.in/v1') 
     .setProject('6a25699200297850cf39')
     .setKey(process.env.ADMIN_API_KEY); // Failsafe authentication
 
-  const databases = new Databases(client)
+  const tablesDb = new TablesDB(client)
 
-  try {
-    await databases.createDocument(
-      '6a256ab3002cc4f2403e',
-      'userprofiles',
-      userId,
-      {
-        userId: userId,
-        preferences: JSON.stringify({ theme: 'dark', notifications: true })
-      }
-    )
-    log(`Profile successfully forged for user ${userId}`)
-    return res.json({ success: true, userId })
-  } catch (err) {
-    error(`Database sync failed for ${userId}: ${err.message}`)
-    return res.json({ success: false, error: err.message}, 500)
+  if (eventHeader.includes('.create')) {
+    try {
+      await tablesDb.createRow({
+        databaseId: '6a256ab3002cc4f2403e',
+        tableId: 'userprofiles',
+        rowId: userId,
+        data: {
+          userId: userId,
+          preferences: JSON.stringify({ theme: 'dark', notifications: true })
+        }
+      })
+      log(`Profile successfully forged for user ${userId}`)
+      return res.json({ success: true, userId })
+    } catch (err) {
+      error(`Database sync failed for ${userId}: ${err.message}`)
+      return res.json({ success: false, error: err.message}, 500)
+    }
+
+  } else if (eventHeader.includes('.delete')) {
+    try {
+      await tablesDb.deleteRow({
+        databaseId: '6a256ab3002cc4f2403e',
+        tableId: 'userprofiles',
+        rowId: userId
+      })
+      log(`Profile successfully deleted for user ${userId}`)
+      return res.json({ success: true, userId })
+    } catch (err) {
+      error(`Database sync failed for ${userId}: ${err.message}`)
+      return res.json({ success: false, error: err.message}, 500)
+    }
+  } else {
+    return res.json({ success: false, message: "Event not handled yet" })
   }
 };
